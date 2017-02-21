1b10db4776df4be5a43f34a915f17784f7db9259
Sunil Antony <sunil.antony@loanstreet.com.my>
2017-02-21 23:07:44 +0800

    Adds controller and routes

--- app/controllers/store_resources_controller.rb
+++ app/controllers/store_resources_controller.rb
@@ -1,74 +1,19 @@
 class StoreResourcesController < ApplicationController
-  before_action :set_store_resource, only: [:show, :edit, :update, :destroy]
 
-  # GET /store_resources
-  # GET /store_resources.json
-  def index
-    @store_resources = StoreResource.all
+  # GET /state
+  def state
+    resource = StoreResource.includes(:state_changes).find_by(
+      resource_type: params[:resource_type],
+      resource_id: params[:resource_id]
+    )
+    render json: resource.state_at(params[:time]), status: 200
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Resource Not Found' }, status: 404
   end
 
-  # GET /store_resources/1
-  # GET /store_resources/1.json
-  def show
+  # GET /resource_ids
+  def resource_ids
+    resource_ids = StoreResource.where(resource_type: params[:resource_type]).pluck(:resource_id)
+    render json: resource_ids, status: :ok
   end
-
-  # GET /store_resources/new
-  def new
-    @store_resource = StoreResource.new
-  end
-
-  # GET /store_resources/1/edit
-  def edit
-  end
-
-  # POST /store_resources
-  # POST /store_resources.json
-  def create
-    @store_resource = StoreResource.new(store_resource_params)
-
-    respond_to do |format|
-      if @store_resource.save
-        format.html { redirect_to @store_resource, notice: 'Store resource was successfully created.' }
-        format.json { render :show, status: :created, location: @store_resource }
-      else
-        format.html { render :new }
-        format.json { render json: @store_resource.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /store_resources/1
-  # PATCH/PUT /store_resources/1.json
-  def update
-    respond_to do |format|
-      if @store_resource.update(store_resource_params)
-        format.html { redirect_to @store_resource, notice: 'Store resource was successfully updated.' }
-        format.json { render :show, status: :ok, location: @store_resource }
-      else
-        format.html { render :edit }
-        format.json { render json: @store_resource.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /store_resources/1
-  # DELETE /store_resources/1.json
-  def destroy
-    @store_resource.destroy
-    respond_to do |format|
-      format.html { redirect_to store_resources_url, notice: 'Store resource was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_store_resource
-      @store_resource = StoreResource.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def store_resource_params
-      params.require(:store_resource).permit(:resource_type, :changed_at, :changed_values)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'store_resources#index'
 
+  get '/resource_ids', to: "store_resources#resource_ids"
+  get '/store_resources/state', to: "store_resources#state"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
